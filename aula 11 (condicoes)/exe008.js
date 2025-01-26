0c7cefa79bbe1074e74012da4114dbbae53de61e
hemillesferreira <hemillesferreira@hotmail.com>
2025-01-25 22:44:32 -0300

    verificxar pais

---
+++ aula 11 (condicoes)/exe008.js
@@ -0,0 +1,9 @@
+let vel = 50.5
+console.log (`a velocidade do seu carro é  ${vel} `)
+if(vel > 60){
+    console.log (`voce ultrapassou a velocidade perminta. MULTADO!`)
+}else{
+    console.log(`digija com cuidado`)
+}
+
+
