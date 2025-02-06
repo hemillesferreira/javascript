560975ec120c4b5c38c3e1e4c9cbfe9a90cefcf5
hemillesferreira <hemillesferreira@hotmail.com>
2025-02-05 22:32:21 -0300

    up

--- aula14ex/script.js
+++ aula14ex/script.js
@@ -1,11 +1,34 @@
-function calcular(){
-    let contador = document.getElementById('cont').value;
-    contador = parseInt(contador)
-    let fim = document.getElementById('txfim').value;
-    fim = parseInt(fim)
-    let passo = document.getElementById('txtpasso').value;
-    passo = parseInt(passo)
-    while(contador<= fim){
-        contador++
+function contar(){
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
+
+    if(ini.value.length == 0 || fim.value. length == 0 || passo.value.length == 0){
+        window.alert('[ERROR!] Preenha os dados')
+        res.innerHTML= 'Impossivel Contar'
+    }else{
+        res.innerHTML= 'contando'
+        let i = Number (ini.value)
+        let f = Number (fim.value)
+        let p = Number (passo.value)
+        if(p<=0){
+            window.alert('Passo invalido')
+            p=1
+        }
+
+        if (i < f) {
+            // Contagem crescente
+            for (let c = i; c <= f; c += p) {
+                res.innerHTML += `${c} 👉 `
+            }
+        } else {
+            // Contagem regressiva
+            for (let c = i; c >= f; c -= p) {
+                res.innerHTML += `${c} 👉 `
+            }
+        }
+
+        res.innerHTML += '🏁'
     }
 }
